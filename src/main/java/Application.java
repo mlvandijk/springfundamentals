59dc5b263990ec8c434ea6ea38518c92605f7ee3
maritvandijk <maritvandijk@bol.com>
2017-09-14 14:06:35 +0200

    Cleanup imports

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,6 @@
-import com.pluralsight.model.Customer;
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.List;
 
 public class Application {
     public static void main(String[] args){
@@ -12,7 +8,7 @@ public class Application {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
-        
+
         System.out.print(service.findAll().get(0).getFirstName());
     }
 }
