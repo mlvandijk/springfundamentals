929aaef467acdb4eca8981ea16309fe288cf1e32
maritvandijk <maritvandijk@bol.com>
2017-09-14 11:48:07 +0200

    Initial commit

---
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -0,0 +1,18 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Customer;
+import com.pluralsight.repository.CustomerRepository;
+import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+
+import java.util.List;
+
+// This is where your business logic will reside
+public class CustomerServiceImpl implements CustomerService {
+
+    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+
+    @Override
+    public List<Customer> findAll(){
+        return customerRepository.findAll(); // hardcoded reference to out repository
+    }
+}
