b4824f8cd3b1d4e7cda80cb9d0ba80854e7567db
maritvandijk <maritvandijk@bol.com>
2017-09-14 16:35:01 +0200

    Use constructor injection

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -10,15 +10,20 @@ import java.util.List;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    @Autowired
+//    @Autowired
     private CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository){
+        System.out.println("We're using constructor injection");
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll(); // hardcoded reference to out repository
     }
 
-    @Autowired
+//    @Autowired
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
