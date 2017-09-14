04ef23422dc6b2a7052ab14a7f669c3c3a49aabf
maritvandijk <maritvandijk@bol.com>
2017-09-14 14:04:18 +0200

    Use Autowired

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,18 @@
 import com.pluralsight.model.Customer;
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args){
 
-        CustomerService service = new CustomerServiceImpl(); // again, hardcoded
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        List<Customer> customers = service.findAll();
-        System.out.print(customers.get(0).getFirstName());
+        CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
+        
+        System.out.print(service.findAll().get(0).getFirstName());
     }
 }
