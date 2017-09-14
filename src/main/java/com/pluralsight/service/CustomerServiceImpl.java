c029fdccd351efc3f4a0f522743ebe9cc5d85039
maritvandijk <maritvandijk@bol.com>
2017-09-14 17:06:00 +0200

    Add Constructor injections vs setter injection

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -10,6 +10,10 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll(); // hardcoded reference to out repository
