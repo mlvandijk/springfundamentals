d1b5bf653fdc20e0d38c5f61649fdbe456cd89b0
maritvandijk <maritvandijk@bol.com>
2017-09-14 13:07:22 +0200

    Add and load Beans

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,19 @@
 import com.pluralsight.model.Customer;
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
 public class Application {
     public static void main(String[] args){
 
-        CustomerService service = new CustomerServiceImpl(); // again, hardcoded
+//        CustomerService service = new CustomerServiceImpl(); // again, hardcoded
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        List<Customer> customers = service.findAll();
-        System.out.print(customers.get(0).getFirstName());
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
+
+        System.out.print(service.findAll().get(0).getFirstName());
     }
 }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -1,18 +1,22 @@
 package com.pluralsight.service;
 
+import java.util.List;
+
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
-import java.util.List;
 
 // This is where your business logic will reside
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
 
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll(); // hardcoded reference to out repository
+    }
+
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 }
