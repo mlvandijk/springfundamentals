8ed242b1351ae6a5db3817f2c67344ba669ec344
Marit van Dijk <maritvandijk@bol.com>
2017-09-21 13:55:12 +0200

    Add app.properties to inject values with java config

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -5,10 +5,18 @@ import com.pluralsight.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan({"com.pluralsight"})
+@PropertySource("app.properties") //Use plural if multiple files
 public class AppConfig {
+
+    @Bean // Takes the properties from the file and makes them available in the context
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 //    @Bean(name = "customerService") // follows bean naming convention
 //    public CustomerService getCustomerService(){
